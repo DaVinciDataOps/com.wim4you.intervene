9fa8459174db9a6c991ba0022d2ab9d7a43da26c
Frank de Pijper <Frank.de.Pijper@ict.nl>
2025-09-13 15:14:43 +0200

    Init

--- app/src/main/java/com/wim4you/intervene/MainActivity.kt
+++ app/src/main/java/com/wim4you/intervene/MainActivity.kt
@@ -58,9 +58,10 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_vigilantes, R.id.nav_settings
+                R.id.nav_home
             ), drawerLayout
         )
+
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
